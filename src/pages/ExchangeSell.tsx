7b6f83cedc0dab7a46b039d543782853665c3a5a
wDrag <haxorus5224@gmail.com>
2024-09-24 02:07:43 +0700

    feat: full router

--- src/pages/ExchangeSell.tsx
+++ src/pages/ExchangeSell.tsx
@@ -1,11 +1,11 @@
-const NullPage = () => {
+const ExchangeSellPage = () => {
   return (
     <div>
       <div className="container mt-20 flex w-full flex-col overflow-y-scroll p-10">
-        <span>404</span>
+        <span>Exchange Sell Page</span>
       </div>
     </div>
   );
 };
 
-export default NullPage;
+export default ExchangeSellPage;

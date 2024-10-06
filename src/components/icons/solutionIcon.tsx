9ee6ce9ea07a268b043c53c2ee9d996b2d05723c
Trần Huy Hoàng <89974827+HuyHoang2358@users.noreply.github.com>
2024-10-06 09:46:48 +0700

    Hoangth33 (#4)
    * fix
    
    * remove Logo
    
    * feat UI homepage

---
+++ src/components/icons/solutionIcon.tsx
@@ -0,0 +1,38 @@
+export const SolutionIcon = () => {
+  return (
+    <svg viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect
+        x="21.2449"
+        y="16.8961"
+        width="16"
+        height="16"
+        transform="rotate(105.771 21.2449 16.8961)"
+        fill="white"
+      />
+      <rect
+        x="52.0404"
+        y="25.5936"
+        width="16"
+        height="16"
+        transform="rotate(105.771 52.0404 25.5936)"
+        fill="white"
+      />
+      <rect
+        x="40.9915"
+        y="5.84729"
+        width="16"
+        height="16"
+        transform="rotate(105.771 40.9915 5.84729)"
+        fill="#A66AFE"
+      />
+      <rect
+        x="32.2941"
+        y="36.6427"
+        width="16"
+        height="16"
+        transform="rotate(105.771 32.2941 36.6427)"
+        fill="#A66AFE"
+      />
+    </svg>
+  );
+};

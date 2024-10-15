3c22d779c9c68d25244b8fd25ed527ce58c51728
wDrag <haxorus5224@gmail.com>
2024-10-15 21:52:43 +0700

    fix: decimals

---
+++ src/constants/tokenData.ts
@@ -0,0 +1,42 @@
+export const collectionList = [
+  {
+    name: 'Aptos Monkeys',
+    ipfsUri: 'ipfs://bafybeig6bepf5ci5fyysxlfefpjzwkfp7sarj6ed2f5a34kowgc6qenjfa/5879.png',
+  },
+  {
+    name: 'The Loonies',
+    ipfsUri: 'ipfs://QmbXf9kadyDi4APLyQcK1d9HJXNteWJKpiXWsfTQxC9ajB',
+  },
+  {
+    name: 'Aptomingos',
+    ipfsUri: 'ipfs://bafybeidlvhvnnnl3zznatgio6gvd3xsss5uszc7fuwzsqn2ttzknosnxly/821.png',
+  },
+  {
+    name: 'Bruh Bears',
+    ipfsUri: 'ipfs://bafybeih6hezn7yyabgunclxhi5xa2sbfd6gnnc6un3lilcufq5yyywlpqm/58.png',
+  },
+  {
+    name: 'Creatus',
+    ipfsUri: 'https://arweave.net/xcVkGlMs_Xd8qYDdOkUISgN7SjaPXaeyVAeI-ktYgqs/10.png',
+  },
+  {
+    name: 'Pontem Space Pirates',
+    ipfsUri: 'ipfs://bafybeic7kh6ah65l7ekydksjvwrf2p2o3xesnagbkgu4j4fbc2red7y4km/644.png',
+  },
+  {
+    name: 'AptoRobos',
+    ipfsUri: 'ipfs://bafybeiektg25bobqfovm3kpncztjko7ud4xp7jrobg4gnmiukjzkrreh7i/25.jpeg',
+  },
+  {
+    name: 'Aptos Creature',
+    ipfsUri: 'ipfs://QmcrCgrajaNydQHtQJVUaHDiEUHkojYFsgjsvNYvB9kPtZ/10.png',
+  },
+  {
+    name: 'Pontem Dark Ages',
+    ipfsUri: 'ipfs://bafybeibf6zin5ji7id5g4qg2zefza3my4sdoph5awcn4dzkzrvfyiv6uke/1.png',
+  },
+  {
+    name: 'Spooks',
+    ipfsUri: 'ipfs://QmTBduDDEJRzXhXUtYwK6YtD4N6keCBXtLKz4p4nex1MHe',
+  },
+];
--- src/lib/utils/tokenData.ts
+++ src/lib/utils/tokenData.ts
@@ -5,6 +5,11 @@ export const getIdFromName = (name: string) => {
   return splited[splited.length - 1];
 };
 
+export const getNameWithoutId = (name: string) => {
+  const splited = name.split('#');
+  return splited[0];
+};
+
 export const toAddress = (name: string) => {
   return AccountAddress.fromString(name);
 };
